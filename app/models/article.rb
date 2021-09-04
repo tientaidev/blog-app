655231d341e8e1bd8fc02436c68f680062547ae9
Tien Tai <Tientaiphan.joe@gmail.com>
2021-09-04 23:22:42 +0700

    Fix errors when creating new blog

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments
 
-  include Visible
-
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-  enum status: %i[PUBLIC ARCHIVED]
-
-  def archive?
-    status == 'ARCHIVED'
-  end
 end
